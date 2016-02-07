dba1e38c08da80284562e2a43d2e40fd3941e123
Deryk Mathew <Spoons@localhost.localdomain>
2016-02-07 17:56:21 +0000

    set/get

---
+++ src/usecaseassistant/UseCaseAssistantMainClass.java
@@ -0,0 +1,46 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package usecaseassistant;
+
+import javax.swing.JFrame;
+
+/**
+ *
+ * @author Deryk Mathew
+ */
+public class UseCaseAssistantMainClass {
+    
+        /**
+     * Create the GUI and show it.  For thread safety,
+     * this method should be invoked from the
+     * event-dispatching thread.
+     */
+    private static void createAndShowGUI() {
+        //Create and set up the window.
+        JFrame frame = new JFrame("Use Case Assistant");
+        frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+
+        UseCaseAssistant mfp = new UseCaseAssistant();
+        frame.getContentPane().add(mfp);
+        
+        //Display the window.
+        frame.pack();
+        frame.setVisible(true);
+    }
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        javax.swing.SwingUtilities.invokeLater(new Runnable() {
+            @Override
+            public void run() {
+                createAndShowGUI();
+            }
+        });
+    }
+    
+}
